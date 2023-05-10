4f2623ded71c02a625c10f5725e1eec65797e161
ISalkaev <133092679+ISalkaev@users.noreply.github.com>
2023-05-10 12:21:35 +0500

    Add files via upload

---
+++ ProjectCalculator.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class ProjectCalculator {
+    public static String calc(String input) throws Exception {
+        String[] tokens = input.split("\\s+");
+        if (tokens.length != 3){
+            throw new RuntimeException("Строка не является математической операцией");
+        }
+        int a = Integer.parseInt(tokens[0]);
+        int b = Integer.parseInt(tokens[2]);
+        char op = tokens[1].charAt(0);
+        int result = 0;
+
+        switch (op){
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                if(b == 0){
+                    throw new Exception("Division by zero");}
+                result = a / b;
+                break;
+            default:
+                throw new Exception("Строка не является математической операцией");
+        }
+        if(a>10){
+            throw new Exception("Цифра выше 10!");
+        }
+        if(b>10){
+            throw new Exception("Цифра выше 10!");
+        }
+        return Integer.toString(result);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("Введите данные, (пример: 1 + 2): ");
+            String input = scanner.nextLine();
+            if(input.equals("exit")){
+                break;
+            }
+            String result = calc(input);
+            System.out.println("Result: "+result);
+
+        }
+    }
+}
